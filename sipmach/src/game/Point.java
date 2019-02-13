e2046858217c0d6736fad1a5dde61a640cbd17f5
Jonas <Jonas@10.4.8.217>
2019-02-13 14:32:41 +0100

    1.it

--- sipmach/src/game/Point.java
+++ sipmach/src/game/Point.java
@@ -3,4 +3,14 @@ package game;
 public class Point {
 	public double x;
 	public double y;
+	
+	Point(){
+		this.x = 0;
+		this.y = 0;
+	}
+	
+	Point(double x, double y){
+		this.x = x;
+		this.y = y;
+	}
 }
