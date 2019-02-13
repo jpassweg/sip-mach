0be73e8f241fe18e0f898be2e3aedd229b361bf6
Jonas <Jonas@10.4.8.217>
2019-02-13 12:25:06 +0100

    succaa 2

--- sipmach/src/game/Main.java
+++ sipmach/src/game/Main.java
@@ -1,7 +1,12 @@
-package sipmach;
+package game;
 
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("hello");
+		su();
+	}
+	
+	static void su() {
+		System.out.println("Suck a dick");
 	}
 }
