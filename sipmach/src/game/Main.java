78b634bb62b5e53022ab40591fb88783097ae69c
Jonas Passweg <jpassweg@student.ethz.ch>
2019-02-13 12:22:16 +0100

    elgloff commit

--- sipmach/src/game/Main.java
+++ sipmach/src/game/Main.java
@@ -1,7 +1,7 @@
-package sipmach;
+package game;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("hello");
+		System.out.println("egloff will never see this");
 	}
 }
