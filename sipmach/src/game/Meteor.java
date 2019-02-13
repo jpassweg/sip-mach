b25e65540a4b7356b136827757dd3958a1e06ff8
Jonas <Jonas@10.4.8.217>
2019-02-13 14:05:56 +0100

    1. it

---
+++ sipmach/src/game/Meteor.java
@@ -0,0 +1,32 @@
+package game;
+
+import java.util.Random;
+
+public class Meteor {
+	
+	private double x;
+	private double y;
+	private double speed;
+	private double phi;
+	private int size;
+	private static final int sizeX = 600;
+	private static final int sizeY = 800;
+	Random rand = new Random();
+	
+	Meteor(){
+		this.x = rand.nextInt(sizeX * (int) 1.4) - sizeX * 0.2;
+		this.y = rand.nextInt(sizeY / 5) + sizeY;
+		this.speed = rand.nextDouble() * 20;
+		this.phi = rand.nextInt(90) - 45;
+		this.size = rand.nextInt(50);
+	}
+	
+	void update() {
+		this.x += speed * Math.sin(phi);
+		this.y += speed * Math.cos(phi);
+	}
+	
+	void show() {
+		
+	}
+}
