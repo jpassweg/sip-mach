c07df2487e7be5ec398b144b12660b9ebf4dccd1
Jonas <Jonas@10.4.8.217>
2019-02-13 16:00:05 +0100

    what even is this

--- sipmach/src/game/Window.java
+++ sipmach/src/game/Window.java
@@ -1,21 +1,35 @@
 package game;
 import java.awt.*;
-import java.util.List;
+import java.util.LinkedList;
 import javax.swing.*;
 import java.lang.InterruptedException;
 public class Window implements Runnable {
 	
 	Window() throws InterruptedException{
-		JPanel panel = new JPanel();
-		panel.setBackground(Color.BLACK);
-		panel.show();
-		while(panel.isVisible()) {
-			panel.wait(50000);
-			
-		}
+		JFrame frame = new JFrame("Hello Bitches");
+		setup(frame);
 	}
 	
+	void setup(JFrame frame) throws InterruptedException{
+		frame.setBackground(Color.BLACK);
+		frame.setSize(600, 800);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		panels(frame);
+	}
 	
+	void panels(JFrame frame) throws InterruptedException {
+		JPanel panel1 = new JPanel();
+		frame.getContentPane().add(panel1);
+		Meteor met = new Meteor();
+		panel1.add(met);
+		frame.setVisible(true);
+		
+		while(true) {
+			met.update();
+			panel1.repaint();
+			try { Thread.sleep(10); } catch (InterruptedException e) { e.printStackTrace(); }
+		}
+	}
 	
 	
 	public void run() 
@@ -24,7 +38,7 @@ public class Window implements Runnable {
 		
     } 
 	
-	public void update(double playerX, List<Point> meteors) {
+	public void update(Player playerX, LinkedList<Meteor> meteors) {
 		
 	}
 }
