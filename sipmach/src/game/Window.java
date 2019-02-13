4fdaab98bc62f7a1f2042ee475883cc90772bb59
Jonas <Jonas@10.4.8.217>
2019-02-13 16:30:23 +0100

    del

--- sipmach/src/game/Window.java
+++ sipmach/src/game/Window.java
@@ -9,6 +9,8 @@ import java.lang.InterruptedException;
 public class Window implements Runnable {
 	
 	JPanel panel;
+	private Player player;
+	private LinkedList<Meteor> meteors;
 	
 	
 	Window() throws InterruptedException{
@@ -29,10 +31,25 @@ public class Window implements Runnable {
 	
 	public void run() 
     { 
-		System.out.println("Window" + Thread.currentThread().getId() + " is running!");
+		
+        Thread tPlayer;
+        player = new Player();
+		player.setParent(this);
+		tPlayer = new Thread(player);
+		tPlayer.start();
+		
+		while(tPlayer.isAlive()) {
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		//window.stop() add synchronized method in window that stops it
+		System.out.println("exit game from object manager");
 		
     } 
-	
 	public void update(Player playerX, LinkedList<Meteor> meteors) {
 		for(int i = 0; i < meteors.size(); i++) {
 			meteors.get(i).update();
