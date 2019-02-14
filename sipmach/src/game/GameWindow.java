024d960f8359d745e5b19e041b7477e345f4385f
Jonas Passweg <jpassweg@student.ethz.ch>
2019-02-14 15:05:01 +0100

    ctrl shift f

--- sipmach/src/game/GameWindow.java
+++ sipmach/src/game/GameWindow.java
@@ -14,7 +14,7 @@ public class GameWindow {
 	int boostCounter;
 	int timeSteps;
 	boolean movable;
-	
+
 	public GameWindow(int screenWidth, int screenHeight) {
 		this.screenWidth = screenWidth;
 		this.screenHeight = screenHeight;
@@ -24,78 +24,73 @@ public class GameWindow {
 		this.timeSteps = 0;
 		this.movable = true;
 	}
-	
+
 	public void run() {
 		player = new Player(screenWidth, screenHeight);
 		rand = new Random();
 		window.setResizable(false);
 		window.open();
-		
+
 		int movement = 0;
-		 
-		while(window.isOpen()) {
-			
-			
+
+		while (window.isOpen()) {
+
 			draw();
-			
-			
-			for(int i = 0; i < meteors.size(); i++) {
+
+			for (int i = 0; i < meteors.size(); i++) {
 				Meteor curr = meteors.get(i);
-				if(Math.sqrt(Math.pow((curr.x - player.x),2) + Math.pow((curr.y - player.y),2)) < curr.size) {
+				if (Math.sqrt(Math.pow((curr.x - player.x), 2) + Math.pow((curr.y - player.y), 2)) < curr.size) {
 					System.out.println("collision: (" + curr.x + "," + curr.y + ") - " + curr.size);
 				}
-				
-				if(curr.y > screenHeight) {
+
+				if (curr.y > screenHeight) {
 					meteors.remove(curr);
 				} else {
 					curr.update();
-				} 
+				}
 			}
 			stepcounter++;
-			if(rand.nextInt(50 - Math.min(45, (int) stepcounter/50)) == 0) {
+			if (rand.nextInt(50 - Math.min(45, (int) stepcounter / 50)) == 0) {
 				meteors.add(new Meteor(screenWidth, screenHeight));
 			}
-			if(stepcounter % 500 == 0 && boostCounter < 5) {
+			if (stepcounter % 500 == 0 && boostCounter < 5) {
 				boostCounter++;
 			}
-			if(!movable) {
+			if (!movable) {
 				timeSteps++;
-				if(timeSteps >= 200) {
+				if (timeSteps >= 200) {
 					timeSteps = 0;
 					movable = true;
 				}
 			}
-			
-			 
+
 			movement = 0;
-			if(movable) {
-				if(window.isKeyPressed("left")) {
+			if (movable) {
+				if (window.isKeyPressed("left")) {
 					movement = -1;
 					boostCounter--;
 					movable = false;
-				} else if(window.isKeyPressed("right")) {
+				} else if (window.isKeyPressed("right")) {
 					movement = 1;
 					boostCounter--;
 					movable = false;
 				}
 			}
 			player.move(movement);
-			
-			
+
 			window.refreshAndClear(5);
-			
-			
+
 		}
 	}
 
 	void draw() {
-		window.setColor(0,0,0);
+		window.setColor(0, 0, 0);
 		window.fillRect(0, 0, window.getWidth(), window.getHeight());
-		window.setColor(139,69,19);
-		for(int i = 0; i < meteors.size(); i++) {
+		window.setColor(139, 69, 19);
+		for (int i = 0; i < meteors.size(); i++) {
 			window.fillCircle(meteors.get(i).x, meteors.get(i).y, meteors.get(i).size);
 		}
-		window.setColor(255,255,255);
+		window.setColor(255, 255, 255);
 		window.fillRect(player.x, player.y, 10, 30);
 		player.drawBoostCounter(window, boostCounter);
 	}
