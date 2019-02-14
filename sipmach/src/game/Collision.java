534262695cf382c6339d85f4006c90a8eb786432
Jonas <Jonas@LAPTOP-FONDILTH>
2019-02-14 17:39:11 +0100

    changed size to radius

--- sipmach/src/game/Collision.java
+++ sipmach/src/game/Collision.java
@@ -7,7 +7,7 @@ public class Collision {
 	static boolean collides(Player player, ArrayList<Meteor> meteors) {
 		for(int i = 0; i < meteors.size(); i++) {
 			if(Math.pow((meteors.get(i).x - player.x), 2) + Math.pow((meteors.get(i).y - player.y), 2)
-			   <= Math.pow((player.playerWidth/2 + meteors.get(i).size), 2)) return true;
+			   <= Math.pow((player.playerWidth/2 + meteors.get(i).radius), 2)) return true;
 		}
 		return false;
 	}
@@ -20,13 +20,13 @@ public class Collision {
 		for (int j = 0; j < meteors.size() - 1; j++) {
 			Meteor met = meteors.get(j);
 			for (int i = 1; i < meteors.size(); i++) {
-				if (met.x + met.size <= meteors.get(i).x - meteors.get(i).size) continue;
-				if (met.x - met.size >= meteors.get(i).x + meteors.get(i).size) continue;
-				if (met.y + met.size <= meteors.get(i).y - meteors.get(i).size) continue;
-				if (met.y - met.size >= meteors.get(i).y + meteors.get(i).size) continue;
+				if (met.x + met.radius <= meteors.get(i).x - meteors.get(i).radius) continue;
+				if (met.x - met.radius >= meteors.get(i).x + meteors.get(i).radius) continue;
+				if (met.y + met.radius <= meteors.get(i).y - meteors.get(i).radius) continue;
+				if (met.y - met.radius >= meteors.get(i).y + meteors.get(i).radius) continue;
 				
 				double d = Math.sqrt(Math.pow((met.x - meteors.get(i).x), 2) + Math.pow((met.y - meteors.get(i).y), 2));
-				if (d < met.size + meteors.get(i).size) {
+				if (d < met.radius + meteors.get(i).radius) {
 					//meteors.add(meteorFusion(met, meteors.get(i), window));
 					meteors.addAll(meteorBreak(met, meteors.get(i), window));
 					meteors.remove(met);
@@ -41,7 +41,7 @@ public class Collision {
 		Meteor m = new Meteor((int)window.getWidth(), (int)window.getHeight());
 		m.x = (met.x + other.x) / 2;
 		m.y = (met.y + other.y)/ 2; 
-		m.size = met.size + other.size;
+		m.radius = met.radius + other.radius;
 		return m;
 	}
 	
@@ -62,29 +62,29 @@ public class Collision {
 	}
 	
 	static void giveStats(Meteor oo, Meteor ot, Meteor to, Meteor tt, Meteor met, Meteor other) {
-		oo.x = met.x + met.size * 3/8;
+		oo.x = met.x + met.radius * 3/8;
 		oo.y = met.y;
 		oo.velX = met.velX;
 		oo.velY = met.velY;
-		oo.size = Math.max(met.size/2, 1);
+		oo.radius = Math.max(met.radius/2, 1);
 		
-		ot.x = met.x - met.size * 3/8;
+		ot.x = met.x - met.radius * 3/8;
 		ot.y = met.y;
 		ot.velX = -met.velX;
 		ot.velY = met.velY;
-		ot.size = Math.max(met.size/2, 1);
+		ot.radius = Math.max(met.radius/2, 1);
 		
-		to.x = other.x + other.size * 3/8;
+		to.x = other.x + other.radius * 3/8;
 		to.y = other.y;
 		to.velX = other.velX;
 		to.velY = other.velY;
-		to.size = Math.max(other.size/2, 1);
+		to.radius = Math.max(other.radius/2, 1);
 		
-		tt.x = other.x - other.size * 3/8;
+		tt.x = other.x - other.radius * 3/8;
 		tt.y = other.y;
 		tt.velX = -other.velX;
 		tt.velY = other.velY;
-		tt.size = Math.max(other.size/2, 1);
+		tt.radius = Math.max(other.radius/2, 1);
 	}
 	
 	
